b5b53554138b570b3c3ffe578216157c548ed41d
홍성민 <seanhong2000@gmail.com>
2023-06-03 20:20:03 +0900

    Initial Commit

---
+++ ShowCase/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ShowCase
+//
+//  Created by Sean Hong on 2023/06/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShowCase/ShowCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowCaseApp.swift
+//  ShowCase
+//
+//  Created by Sean Hong on 2023/06/03.
+//
+
+import SwiftUI
+
+@main
+struct ShowCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
